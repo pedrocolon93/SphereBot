5604211b8316503ee3ee2b83a394f0a5f5bf8ee2
fab <fab@mail.ubuntupedro.com>
2016-11-09 12:21:55 -0500

    Successful  analog reading

---
+++ atttinysoftware/lightsensorreader/sensorreader.44.c
@@ -0,0 +1,80 @@
+// main.c
+//
+// A simple blinky program for ATtiny85
+// Connect red LED at pin 2 (PB3)
+//
+// electronut.in
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <math.h>
+
+#define LOW 0
+#define HIGH 1
+
+#define INPUT(port,pin) DDR ## port &= ~(1<<pin)
+#define OUTPUT(port,pin) DDR ## port |= (1<<pin)
+#define CLEAR(port,pin) PORT ## port &= ~(1<<pin)
+#define SET(port,pin) PORT ## port |= (1<<pin)
+#define TOGGLE(port,pin) PORT ## port ^= (1<<pin)
+#define READ(port,pin) (PIN ## port & (1<<pin))
+
+
+// initialize adc
+void adc_init()
+{
+    // AREF = AVcc
+    ADMUX = (1<<REFS0);
+ 
+    // ADC Enable and prescaler of 128
+    // 16000000/128 = 125000
+    ADCSRA = (1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
+}
+ 
+// read adc value
+uint16_t adc_read(uint8_t ch)
+{
+    // select the corresponding channel 0~7
+    // ANDing with '7' will always keep the value
+    // of 'ch' between 0 and 7
+    ch &= 0b00000111;  // AND operation with 7
+    ADMUX = (ADMUX & 0xF8)|ch;     // clears the bottom 3 bits before ORing
+ 
+    // start single conversion
+    // write '1' to ADSC
+    ADCSRA |= (1<<ADSC);
+ 
+    // wait for conversion to complete
+    // ADSC becomes '0' again
+    // till then, run loop continuously
+    while(ADCSRA & (1<<ADSC));
+ 
+    return (ADC);
+}
+ 
+int main()
+{
+    uint16_t adc_result0;
+    char int_buffer[10];
+
+    // initialize adc and lcd
+    adc_init();
+
+    _delay_ms(50);
+    OUTPUT(B,2);
+    while(1)
+    {
+        adc_result0 = adc_read(3);      // read adc value at PA0
+           
+        if(adc_result0 < 256)
+		{
+			CLEAR(B,2);
+		}
+
+		else
+		{
+			SET(B,2);
+		}
+	
+    }
+}
