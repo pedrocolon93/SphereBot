d8b15233107c5bf41b108581942010aa003300e0
Pedro Colon <pedrocolon93@gmail.com>
2016-11-23 08:36:00 -0500

    More software

---
+++ atttinysoftware/ledstrip/button.44.c
@@ -0,0 +1,45 @@
+// main.c
+//
+// A simple blinky program for ATtiny85
+// Connect red LED at pin 2 (PB3)
+//
+// electronut.in
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <math.h>
+
+#define LOW 0
+#define HIGH 1
+
+#define INPUT(port,pin) DDR ## port &= ~(1<<pin)
+#define OUTPUT(port,pin) DDR ## port |= (1<<pin)
+#define CLEAR(port,pin) PORT ## port &= ~(1<<pin)
+#define SET(port,pin) PORT ## port |= (1<<pin)
+#define TOGGLE(port,pin) PORT ## port ^= (1<<pin)
+#define READ(port,pin) (PIN ## port & (1<<pin))
+
+int main (void)
+{
+    // set PB3 to be output
+
+//    OUTPUT(D, 3); // port D, pin 3 as output
+//    SET(D, 3); // set port D pin 3 to HIGH
+//    CLEAR(D, 3); // set it to LOW
+//
+//    INPUT(B, 5);
+//    if (READ(B, 5) == HIGH)
+    OUTPUT(A,0);
+
+    PORTA = 0x00;
+    while (1) {
+        
+        
+		SET(A,0);
+		
+
+	}
+    
+
+return 1;
+}
